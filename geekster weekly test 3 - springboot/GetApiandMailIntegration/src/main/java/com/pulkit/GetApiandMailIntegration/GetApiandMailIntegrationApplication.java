ef9839eb2912cc4e56b2b89f07dc6ebcb3e6fcd8
pulkit mittal <pulkitmittal194@gmail.com>
2023-06-18 17:50:07 +0530

    springboot weekly assignment 3

---
+++ geekster weekly test 3 - springboot/GetApiandMailIntegration/src/main/java/com/pulkit/GetApiandMailIntegration/GetApiandMailIntegrationApplication.java
@@ -0,0 +1,14 @@
+package com.pulkit.GetApiandMailIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetApiandMailIntegrationApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(GetApiandMailIntegrationApplication.class, args);
+	}
+
+}
---
+++ geekster weekly test 3 - springboot/GetApiandMailIntegration/src/main/java/com/pulkit/GetApiandMailIntegration/MyObject.java
@@ -0,0 +1,24 @@
+package com.pulkit.GetApiandMailIntegration;
+
+public class MyObject {
+
+    private String name;
+    private int age;
+
+
+    public MyObject(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "MyObject{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+
